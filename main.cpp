8ab6a98f6fc3e38c5fc213bd42cdf1aaf6e186d6
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2024-12-01 17:24:42 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+# include<iostream>
+using nmaespace .std;
+int main()
+{
+ // cout<<"himanshi malviya";
+    // cout<<endl;
+    // cout<<"cybrom";
+    // cout<<"\n";
+    // cout<<"institution";
+    // <--------- printing numbers-------------->
+    // cout<<4;
+    // cout<<endl;
+    // cout<<"4";
+    // cout<<endl;
+    // cout<<4+3;
+    // cout<<"\n";
+    // cout<<"4+3";
+// <--------------------variable--------------------->
+// int x;// declarationint 
+// int y;//;intialization
+// x=5;
+// y=6;
+// cout<<x+y;
+int x=5,y=7;
+cout<<x*y;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
